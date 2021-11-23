560fc00a670f0cf6741931946e1efc9eaaa72119
Hardy Pottinger <hardy.pottinger@gmail.com>
2021-11-23 15:48:35 -0600

    modifying homepage.spec.ts to check for the correct text

--- cypress/integration/homepage.spec.ts
+++ cypress/integration/homepage.spec.ts
@@ -6,8 +6,8 @@ describe('Homepage', () => {
     cy.visit('/');
   });
 
-  it('should display translated title "DSpace Angular :: Home"', () => {
-    cy.title().should('eq', 'DSpace Angular :: Home');
+  it('should display translated title "eScholarship DSpace Pilot :: Home"', () => {
+    cy.title().should('eq', 'eScholarship DSpace Pilot :: Home');
   });
 
   it('should contain a news section', () => {
