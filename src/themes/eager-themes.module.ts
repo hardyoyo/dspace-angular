03211aa3bb2b86ba32a16407c2c8265fd4a83596
Hardy Pottinger <hardy.pottinger@gmail.com>
2022-08-10 16:15:37 -0500

    PUBD-641 - correctly configure eager-themes.module.ts

--- src/themes/eager-themes.module.ts
+++ src/themes/eager-themes.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme.module';
+import { EagerThemeModule as CDLEagerThemeModule } from './cdl/eager-theme.module';
 // import { EagerThemeModule as CustomEagerThemeModule } from './custom/eager-theme.module';
 
 /**
@@ -11,7 +11,6 @@ import { EagerThemeModule as DSpaceEagerThemeModule } from './dspace/eager-theme
  */
 @NgModule({
   imports: [
-    DSpaceEagerThemeModule,
     CDLEagerThemeModule,
   ],
 })
